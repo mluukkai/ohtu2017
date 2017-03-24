c1cb5d0f9d5ef241275249e0db3a8fdec37cf54d
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-03-24 18:34:46 +0200

    tehtävä 10

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -2,11 +2,18 @@ package ohtu;
 
 import ohtu.verkkokauppa.Kauppa;
 import ohtu.verkkokauppa.Kirjanpito;
+import ohtu.verkkokauppa.Pankki;
+import ohtu.verkkokauppa.Varasto;
+import ohtu.verkkokauppa.Viitegeneraattori;
 
 public class Main {
 
     public static void main(String[] args) {
-        Kauppa kauppa = new Kauppa();
+        Kirjanpito kirjanpito = new Kirjanpito();
+        Varasto varasto = new Varasto(kirjanpito);
+        Pankki pankki = new Pankki(kirjanpito);
+        Viitegeneraattori viitegen = new Viitegeneraattori();
+        Kauppa kauppa = new Kauppa(varasto, pankki, viitegen);
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
@@ -25,7 +32,7 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
-        for (String tapahtuma : Kirjanpito.getInstance().getTapahtumat()) {
+        for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
     }
--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -2,16 +2,23 @@ package ohtu.verkkokauppa;
 
 public class Kauppa {
 
-    private Varasto varasto;
-    private Pankki pankki;
+    private VarastoInterface varasto;
+    private PankkiInterface pankki;
     private Ostoskori ostoskori;
-    private Viitegeneraattori viitegeneraattori;
+    private VGInterface viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+//    public Kauppa() {
+//        varasto = Varasto.getInstance();
+//        pankki = Pankki.getInstance();
+//        viitegeneraattori = Viitegeneraattori.getInstance();
+//        kaupanTili = "33333-44455";
+//    }
+    
+    public Kauppa(VarastoInterface varasto, PankkiInterface pankki, VGInterface vg) {
+        this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = vg;
         kaupanTili = "33333-44455";
     }
 
