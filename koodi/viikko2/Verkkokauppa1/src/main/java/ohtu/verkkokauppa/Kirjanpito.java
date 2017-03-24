82ddb0e43b8cabb5c051734d2cadcbbfb760a1cd
Joonas Maunu <joonas.maunu@gmail.com>
2017-03-24 15:01:55 +0200

    Verkkokauppa päivitetty

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -2,21 +2,11 @@
 package ohtu.verkkokauppa;
 
 import java.util.ArrayList;
-
 public class Kirjanpito {
-    private static Kirjanpito instance;
-    
-    public static Kirjanpito getInstance() {
-        if ( instance==null) {
-            instance = new Kirjanpito();
-        }
-        
-        return instance;
-    }
     
     private ArrayList<String> tapahtumat;
 
-    private Kirjanpito() {
+    public Kirjanpito() {
         tapahtumat = new ArrayList<String>();
     }
     
--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,22 +1,17 @@
 package ohtu.verkkokauppa;
 
-public class Pankki {
 
-    private static Pankki instanssi;
+public class Pankki implements PankkiInterface {
 
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
 
-        return instanssi;
-    }
     private Kirjanpito kirjanpito;
 
-    public Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
+
+    public Pankki(Kirjanpito kirjanpito) {
+       this.kirjanpito = kirjanpito;
     }
 
+    @Override
     public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
         kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
                 + " viite " + viitenumero + " summa " + summa + "e");
