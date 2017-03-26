7e6d513320786f0c297a9026aa6db9b6dd2e6165
Jukka Peinola <jpeinola@cs.helsinki.fi>
2017-03-26 23:23:21 +0300

    Verkkokauppa springillä

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -5,15 +5,20 @@ import ohtu.verkkokauppa.Kirjanpito;
 import ohtu.verkkokauppa.Pankki;
 import ohtu.verkkokauppa.Varasto;
 import ohtu.verkkokauppa.Viitegeneraattori;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         Kirjanpito kirjanpito      = new Kirjanpito();
-        Varasto varasto            = new Varasto(kirjanpito);
-        Pankki pankki              = new Pankki(kirjanpito);
-        Viitegeneraattori viitegen = new Viitegeneraattori();
-        Kauppa kauppa              = new Kauppa(varasto, pankki, viitegen);
+//        Varasto varasto            = new Varasto(kirjanpito);
+//        Pankki pankki              = new Pankki(kirjanpito);
+//        Viitegeneraattori viitegen = new Viitegeneraattori();
+//        Kauppa kauppa              = new Kauppa(varasto, pankki, viitegen);
+    ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+ 
+    Kauppa kauppa = ctx.getBean(Kauppa.class);
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
