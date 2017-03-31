34ed12ac9bb8d35280d960ffdad583592b9fb58c
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-03-31 21:49:20 +0300

    kansiot viikko1 ja viikko2 lisätty

---
+++ NiittylaJuho/viikko2/NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
@@ -0,0 +1,21 @@
+package ohtuesimerkki;
+
+public class Main {
+    public static void main(String[] args) {
+        Reader reader = new PlayerReader("http://nhlstatistics.herokuapp.com/players.txt");
+        Statistics stats = new Statistics(reader);
+        
+          
+        System.out.println("Philadelphia Flyers");
+        for (Player player : stats.team("PHI") ) {
+            System.out.println( player );
+        }
+        
+        System.out.println("");
+        
+        System.out.println("Top scorers");
+        for (Player player : stats.topScorers(10) ) {
+            System.out.println( player );
+        }        
+    }
+}
