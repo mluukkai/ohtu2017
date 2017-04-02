1064cff451e713fd0f07ceaa76d594fd57e911cf
Paavo Huhtala <paavo.huhtala@gmail.com>
2017-04-02 19:43:58 +0300

    Tee checkstyle-tehtävä

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -6,26 +6,13 @@ import org.springframework.context.support.*;
 
 public class Main {
 
+    public static final int CASE_OF_BEER = 24;
+
     public static void main(String[] args) {
         ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
 
         Kauppa kauppa = ctx.getBean(Kauppa.class);
-
-        // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
-        kauppa.aloitaAsiointi();
-        kauppa.lisaaKoriin(1);
-        kauppa.lisaaKoriin(3);
-        kauppa.lisaaKoriin(3);
-        kauppa.poistaKorista(1);
-        kauppa.tilimaksu("Pekka Mikkola", "1234-12345");
-
-        // seuraava asiakas
-        kauppa.aloitaAsiointi();
-        for (int i = 0; i < 24; i++) {
-            kauppa.lisaaKoriin(5);
-        }
-
-        kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
+        addTestData(kauppa);
 
         AbstraktiKirjanpito kirjanpito = ctx.getBean(AbstraktiKirjanpito.class);
 
@@ -33,5 +20,21 @@ public class Main {
         for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
+    }
+
+    private static void addTestData(Kauppa kauppa) {
+        // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(3);
+        kauppa.lisaaKoriin(3);
+        kauppa.poistaKorista(1);
+        kauppa.tilimaksu("Pekka Mikkola", "1234-12345");
+        // seuraava asiakas
+        kauppa.aloitaAsiointi();
+        for (int i = 0; i < CASE_OF_BEER; i++) {
+            kauppa.lisaaKoriin(5);
+        }
+        kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
     }
 }
