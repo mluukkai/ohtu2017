a7112a29580d79deb1de30a7abbf73357a61a002
Jukka Peinola <jpeinola@cs.helsinki.fi>
2017-04-02 23:41:37 +0300

    Viikko 3 done

--- koodi/viikko2/RiippuvuuksienInjektointi1/src/main/java/ohtu/Laskin.java
+++ koodi/viikko2/RiippuvuuksienInjektointi1/src/main/java/ohtu/Laskin.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 
 public class Laskin {
 
+    public static final int POIS = -9999;
+
     private Scanner lukija;
 
     public Laskin() {
         lukija = new Scanner(System.in);
     }
-    
+
     public void suorita(){
         while( true ) {
             System.out.println("luku 1: ");
             int luku1 = lukija.nextInt();
-            if ( luku1==-9999  ) return;
-            
+            if ( luku1== POIS  ) return;
+
             System.out.println("luku 2: ");
             int luku2 = lukija.nextInt();
-            if ( luku2==-9999  ) return;
-            
+            if ( luku2== POIS  ) return;
+
             int vastaus = laskeSumma(luku1, luku2);
             System.out.println("summa: "+ vastaus);
         }
@@ -27,7 +29,6 @@ public class Laskin {
 
     private int laskeSumma(int luku1, int luku2) {
         return luku1+luku2;
-    }    
-   
-}
+    }
 
+}
--- koodi/viikko2/RiippuvuuksienInjektointi1/src/main/java/ohtu/laskin/Laskin.java
+++ koodi/viikko2/RiippuvuuksienInjektointi1/src/main/java/ohtu/laskin/Laskin.java
@@ -2,21 +2,22 @@ package ohtu.laskin;
 
 public class Laskin {
     private IO io;
+    public static final int POIS = -9999;
 
     public Laskin(IO io) {
         this.io = io;
     }
-    
+
     public void suorita(){
         while( true ) {
             io.print("luku 1: ");
             int luku1 = io.nextInt();
-            if ( luku1==-9999  ) return;
-            
+            if ( luku1==POIS  ) return;
+
             io.print("luku 2: ");
-            int luku2 = io.nextInt();           
-            if ( luku2==-9999 ) return;
-            
+            int luku2 = io.nextInt();
+            if ( luku2==POIS ) return;
+
             int vastaus = laskeSumma(luku1, luku2);
             io.print("summa: "+vastaus+"\n");
         }
@@ -24,6 +25,5 @@ public class Laskin {
 
     private int laskeSumma(int luku1, int luku2) {
         return luku1+luku2;
-    }      
-   
+    }
 }
