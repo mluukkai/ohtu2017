dc8704d3ff2d9601d2d8ba7c7f3c187f279118f8
villepaa <ville.paavola@gmail.com>
2017-04-08 22:37:45 +0300

    tehtävä 7

--- koodi/viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ koodi/viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -21,21 +21,7 @@ public class Stepdefs {
         element.click();          
     } 
 
-    @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
-    public void username_and_password_are_given(String username, String password) throws Throwable {
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(username);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("login"));
-        element.submit();  
-    }
-
-    @Then("^system will respond \"([^\"]*)\"$")
-    public void system_will_respond(String pageContent) throws Throwable {
-        assertTrue(driver.getPageSource().contains(pageContent));
-    }
-    
+  
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_correct_and_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
@@ -43,6 +29,11 @@ public class Stepdefs {
 
     @When("^correct username \"([^\"]*)\" and incorrect password \"([^\"]*)\" are given$")
     public void username_and_incorrect_password_are_given(String username, String password) throws Throwable {
+        logInWith(username, password);
+    }
+    
+    @When("^nonexistent username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
+    public void nonexistent_username_and_irrelevant_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
     
