1c949e6a44dfaa7716fe82fae1d7412f8bcbc3c5
ekalaja <eero.kalaja@helsinki.fi>
2017-04-08 12:53:39 +0300

    teht 7

--- koodi/viikko4/WebLogin/src/test/java/ohtu/ServerRule.java
+++ koodi/viikko4/WebLogin/src/test/java/ohtu/ServerRule.java
@@ -15,7 +15,7 @@ public class ServerRule extends ExternalResource {
 
     @Override
     protected void before() throws Throwable {
-        System.setProperty("webdriver.chrome.driver", "/Users/Desktop/chromedriver"); 
+//        System.setProperty("webdriver.chrome.driver", "/Users/Desktop/chromedriver"); 
         Spark.port(port);
         UserDao dao = new UserDaoForTests();
         dao.add(new User("jukka", "akkuj"));
--- koodi/viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ koodi/viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -21,20 +21,20 @@ public class Stepdefs {
         element.click();          
     } 
 
-    @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
-    public void username_and_password_are_given(String username, String password) throws Throwable {
-        WebElement element = driver.findElement(By.name("username"));
-        element.sendKeys(username);
-        element = driver.findElement(By.name("password"));
-        element.sendKeys(password);
-        element = driver.findElement(By.name("login"));
-        element.submit();  
-    }
+//    @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
+//    public void username_and_password_are_given(String username, String password) throws Throwable {
+//        WebElement element = driver.findElement(By.name("username"));
+//        element.sendKeys(username);
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys(password);
+//        element = driver.findElement(By.name("login"));
+//        element.submit();  
+//    }
 
-    @Then("^system will respond \"([^\"]*)\"$")
-    public void system_will_respond(String pageContent) throws Throwable {
-        assertTrue(driver.getPageSource().contains(pageContent));
-    }
+//    @Then("^system will respond \"([^\"]*)\"$")
+//    public void system_will_respond(String pageContent) throws Throwable {
+//        assertTrue(driver.getPageSource().contains(pageContent));
+//    }
     
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_correct_and_password_are_given(String username, String password) throws Throwable {
@@ -43,6 +43,11 @@ public class Stepdefs {
 
     @When("^correct username \"([^\"]*)\" and incorrect password \"([^\"]*)\" are given$")
     public void username_and_incorrect_password_are_given(String username, String password) throws Throwable {
+        logInWith(username, password);
+    }
+    
+    @When("^incorrect username \"([^\"]*)\" and incorrect password \"([^\"]*)\" are given$")
+    public void incorrect_username_and_incorrect_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
     
