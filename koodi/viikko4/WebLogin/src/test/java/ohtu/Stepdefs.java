f6cb6929435342982e7fcc7f57c5a7e9b49cdbdc
villepaa <ville.paavola@gmail.com>
2017-04-08 23:56:21 +0300

    tehtävä 8

--- koodi/viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ koodi/viikko4/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -19,8 +19,44 @@ public class Stepdefs {
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();          
+    }
+    
+    @Given("^register new user is selected$")
+    public void register_new_user_selected() throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();          
     } 
-
+    
+    @When("^valid username \"([^\"]*)\" and valid password \"([^\"]*)\" and matching confirmation \"([^\"]*)\" are given$")
+    public void valid_username_and_valid_password_and_matching_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        signInWith(username, password, confirmation);
+    }
+    
+    @When("^too short username \"([^\"]*)\" and valid password \"([^\"]*)\" and matching confirmation \"([^\"]*)\" are given$")
+    public void too_short_username_and_valid_password_and_matching_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        signInWith(username, password, confirmation);
+    }
+    
+    @When("^valid username \"([^\"]*)\" and too short password \"([^\"]*)\" and matching confirmation \"([^\"]*)\" are given$")
+    public void correct_username_and_too_short_password_and_matching_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        signInWith(username, password, confirmation);
+    }
+    
+    @When("^valid username \"([^\"]*)\" and only letters password \"([^\"]*)\" and matching confirmation \"([^\"]*)\" are given$")
+    public void correct_username_and_only_letters_password_and_matching_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        signInWith(username, password, confirmation);
+    }
+    
+    @When("^already taken username \"([^\"]*)\" and valid password \"([^\"]*)\" and matching confirmation \"([^\"]*)\" are given$")
+    public void already_taken_username_and_valid_password_and_matching_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        signInWith(username, password, confirmation);
+    }
+    
+    @When("^valid username \"([^\"]*)\" and valid password \"([^\"]*)\" and non matching confirmation \"([^\"]*)\" are given$")
+    public void valid_username_and_valid_password_and_non_matching_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        signInWith(username, password, confirmation);
+    }
   
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_correct_and_password_are_given(String username, String password) throws Throwable {
@@ -35,6 +71,17 @@ public class Stepdefs {
     @When("^nonexistent username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void nonexistent_username_and_irrelevant_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
+    }
+    
+    @Then("^new user is registered$")
+    public void new_user_is_registered() throws Throwable {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+    
+    @Then("^new user is not created and error \"([^\"]*)\" is reported$")
+    public void new_user_is_not_registered_and_error_is_reported(String error) throws Throwable {
+        pageHasContent(error);
+        pageHasContent("Create username and give password");
     }
     
     @Then("^user is logged in$")
@@ -67,5 +114,17 @@ public class Stepdefs {
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
         element.submit();  
-    } 
+    }
+    
+    private void signInWith(String username,String password,String confirmation){
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(confirmation);
+        element = driver.findElement(By.name("signup"));
+        element.submit();  
+    }
 }
---
+++ koodi/viikko4/WebLogin/src/test/resources/ohtu/new_user.feature
@@ -0,0 +1,31 @@
+Feature: A new user account can be created if a proper unused username and password are given
+
+Scenario: creation successful with correct username and password
+        Given register new user is selected
+        When  valid username "otto" and valid password "ottootto5%" and matching confirmation "ottootto5%" are given
+        Then  new user is registered
+
+Scenario: creation fails with too short username and valid password
+    Given register new user is selected
+    When too short username "ot" and valid password "ottootto5%" and matching confirmation "ottootto5%" are given
+    Then new user is not created and error "username should have at least 3 characters" is reported
+
+Scenario: creation fails with correct username and too short password
+    Given register new user is selected
+    When valid username "otto" and too short password "otto%" and matching confirmation "otto%" are given
+    Then new user is not created and error "password should have at least 8 characters" is reported   
+
+Scenario: creation fails with correct username and password consisting of letters
+    Given register new user is selected
+    When valid username "otto" and only letters password "ottootto" and matching confirmation "ottootto" are given
+    Then new user is not created and error "password can not contain only letters" is reported 
+
+Scenario: creation fails with already taken username and valid pasword
+    Given register new user is selected
+    When already taken username "jukka" and valid password "ottootto%" and matching confirmation "ottootto%" are given
+    Then new user is not created and error "username is already taken" is reported 
+
+Scenario: creation fails when password and password confirmation do not match
+    Given register new user is selected
+    When valid username "otto" and valid password "ottootto%" and non matching confirmation "ottoott%" are given
+    Then new user is not created and error "password and password confirmation do not match" is reported   
