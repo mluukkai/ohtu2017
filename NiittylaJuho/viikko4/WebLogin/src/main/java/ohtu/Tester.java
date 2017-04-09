5e9166b97430bebf79f288bb3bf17a0e463a869e
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-04-09 21:53:33 +0300

    tehtävät x-6

--- NiittylaJuho/viikko4/WebLogin/src/main/java/ohtu/Tester.java
+++ NiittylaJuho/viikko4/WebLogin/src/main/java/ohtu/Tester.java
@@ -8,14 +8,73 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/jjniitty/kurssit/chromedriver/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
-        
+
+        sleep(2);
+
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("devnul");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("devnull");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("devnull");
+
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+
+        element.submit();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
         sleep(2);
         
-        WebElement element = driver.findElement(By.linkText("login"));
+        
+        
+        
+
+        element = driver.findElement(By.linkText("login"));
         element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("fjufhsl");
+        element = driver.findElement(By.name("login"));
+
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("devone");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("fjufhsl");
+        element = driver.findElement(By.name("login"));
+
+        sleep(2);
+        element.submit();
+
+        sleep(3);
 
         sleep(2);
 
@@ -24,18 +83,19 @@ public class Tester {
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-        
+
         sleep(2);
         element.submit();
 
         sleep(3);
-        
+
         driver.quit();
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
     }
 }
