a3b79b602987fb318256058848060a654dd84132
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-04-09 23:52:43 +0300

    tehtävä 8

--- NiittylaJuho/viikko4/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
+++ NiittylaJuho/viikko4/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
@@ -22,22 +22,50 @@ public class AuthenticationService {
 
         return false;
     }
-
-    public CreationStatus createUser(String username, String password, String passwordConfirmation) {
+    
+    public CreationStatus createUser(String username, String password, String confirmation) {
         CreationStatus status = new CreationStatus();
-        
+
+        if(invalid(username, password, confirmation, status).errors().isEmpty()){
+        userDao.add(new User(username, password));
+        }
+
+        return status;
+    }
+
+    private CreationStatus invalid(String username, String password, String confirmation, CreationStatus status) {
+        // validity check of username and password
+        if (username.length() < 3) {
+            status.addError("username should have atleast 3 characters");
+        } 
+            
+           if(password.length() < 8) {
+            status.addError("password should have at least 8 characters");
+        }
         if (userDao.findByName(username) != null) {
             status.addError("username is already taken");
         }
-
-        if (username.length()<3 ) {
-            status.addError("username should have at least 3 characters");
+        for (int i = 0; i < username.length(); i++) {
+            Character x = username.charAt(i);
+            if (Character.getType(x) != Character.LOWERCASE_LETTER) {
+                status.addError("username should contain letters a-z only");
+            }
         }
+        boolean eiErikoisia = true;
+        for (int j = 0; j < password.length(); j++) {
+            Character x = password.charAt(j);
+            if (Character.getType(x) != Character.LOWERCASE_LETTER && Character.getType(x) != Character.UPPERCASE_LETTER) {
+                eiErikoisia = false;
 
-        if (status.isOk()) {
-            userDao.add(new User(username, password));
+            }
+        }
+        if(eiErikoisia){
+            status.addError("password can not contain only letters");
         }
         
+        if(password != confirmation){
+            status.addError("password does not match password confirmation");
+        }
         return status;
     }
 
