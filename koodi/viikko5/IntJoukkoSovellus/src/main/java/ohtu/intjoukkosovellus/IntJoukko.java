172f10e47e3a8ad176420e5df28f8352eba5d2cb
villepaa <ville.paavola@gmail.com>
2017-04-18 21:24:24 +0300

    teht4

--- koodi/viikko5/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ koodi/viikko5/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -37,16 +37,13 @@ public class IntJoukko {
 
     public boolean lisaa(int luku) {
         if (!isInArray(luku)) {
-            if(alkioidenLkm == lukujono.length){
-                expandArray();
-            }
-            lukujono[alkioidenLkm] = luku;
-            alkioidenLkm++;
-            return true;
+            hoidaLisaaminen(luku);
         }
         return false;
     }
-
+    
+    
+    
     public int[] getLukujono() {
         if(lukujono == null){
             return null;
@@ -57,15 +54,7 @@ public class IntJoukko {
     public void setLukujono(int[] lukujono) {
         this.lukujono = lukujono;
     }
-    
-    
-    
-    private boolean expandArray(){
-        int[] taulukkoOld = Arrays.copyOf(lukujono,lukujono.length);
-        lukujono = new int[alkioidenLkm + kasvatuskoko];
-        copyExcistingInts(taulukkoOld, lukujono);
-        return true;
-    }
+        
     
     public boolean isInArray(int luku) {
         for (int i = 0; i < alkioidenLkm; i++) {
@@ -79,20 +68,95 @@ public class IntJoukko {
     public boolean poista(int luku) {
         for (int i = 0; i < alkioidenLkm; i++) {
             if (luku == lukujono[i]) {
-                lukujono[i] = 0;
-                fixLukujonoAfterDelete(i+1);
-                alkioidenLkm--;
-                return true;
+                return fixLukujono(i);
             }
         }
         return false;    
     }
     
-    private void fixLukujonoAfterDelete(int index){
-        for (int i = index; i < alkioidenLkm; i++) {
-            lukujono[i-1] = lukujono[i];
-            
+    
+    public int mahtavuus() {
+        return alkioidenLkm;
+    }
+        
+
+    public IntJoukko yhdiste(IntJoukko b) {
+        IntJoukko tama = new IntJoukko();
+        tama.setLukujono(lukujono);
+        tama.setAlkioidenLkm(alkioidenLkm);
+        return yhdista(tama,b.getLukujono());
+    }
+    
+    
+    public IntJoukko leikkaus(IntJoukko b) {
+        IntJoukko tama = new IntJoukko();
+        tama.setLukujono(lukujono);
+        tama.setAlkioidenLkm(alkioidenLkm);
+        return leikkaa(tama,b);
+    }
+    
+       
+    public IntJoukko erotus (IntJoukko b) {
+        IntJoukko tama = new IntJoukko();
+        tama.setLukujono(lukujono);
+        tama.setAlkioidenLkm(alkioidenLkm);
+        return erota(tama,b.getLukujono());
+    }
+    
+     private IntJoukko leikkaa(IntJoukko joukko, IntJoukko toinenJoukko){
+        for (int i = 0; i < lukujono.length; i++) {
+            if(!toinenJoukko.isInArray(lukujono[i])){
+                joukko.poista(lukujono[i]);
+            }
         }
+        return joukko;
+    }
+     
+    private IntJoukko yhdista(IntJoukko joukko, int [] toinenLukujono){
+        for (int i = 0; i < toinenLukujono.length; i++) {
+            if(!isInArray(toinenLukujono[i])){
+                joukko.lisaa(toinenLukujono[i]);
+            }
+        }
+        return joukko;
+    } 
+    
+    private IntJoukko erota(IntJoukko joukko, int [] toinenLukujono){
+       for (int i = 0; i < toinenLukujono.length; i++) {
+            if(isInArray(toinenLukujono[i])){
+                joukko.poista(toinenLukujono[i]);
+            }
+        }
+        return joukko;
+    }
+    
+    private boolean fixLukujono(int index){
+        lukujono[index] = 0;
+        for (int i = index+1; i < alkioidenLkm; i++) {
+            lukujono[i-1] = lukujono[i];
+        }
+        alkioidenLkm--;
+        return true;
+    }
+
+    private boolean expandArray(){
+        int[] taulukkoOld = Arrays.copyOf(lukujono,lukujono.length);
+        lukujono = new int[alkioidenLkm + kasvatuskoko];
+        copyExcistingInts(taulukkoOld, lukujono);
+        return true;
+    }
+    
+    private boolean hoidaLisaaminen(int luku){
+        if(alkioidenLkm == lukujono.length){
+                expandArray();
+            }
+            lukujono[alkioidenLkm] = luku;
+            alkioidenLkm++;
+            return true;
+    }
+    
+    private void setAlkioidenLkm(int lkm){
+        alkioidenLkm = lkm;
     }
 
     private void copyExcistingInts(int[] old, int[]luvut) {
@@ -100,15 +164,7 @@ public class IntJoukko {
             luvut[i] = old[i];
         }
     }
-
-    public int mahtavuus() {
-        return alkioidenLkm;
-    }
     
-    private void setAlkioidenLkm(int lkm){
-        alkioidenLkm = lkm;
-    }
-
     @Override
     public String toString() {
         if (alkioidenLkm == 0) {
@@ -134,54 +190,6 @@ public class IntJoukko {
         }
         tuotos += lukujono[alkioidenLkm - 1] +  "}";
         return tuotos;
-    }
-
-    public IntJoukko yhdiste(IntJoukko b) {
-        IntJoukko tama = new IntJoukko();
-        tama.setLukujono(lukujono);
-        tama.setAlkioidenLkm(alkioidenLkm);
-        return yhdista(tama,b.getLukujono());
-    }
-    
-    private IntJoukko yhdista(IntJoukko joukko, int [] toinenLukujono){
-        for (int i = 0; i < toinenLukujono.length; i++) {
-            if(!isInArray(toinenLukujono[i])){
-                joukko.lisaa(toinenLukujono[i]);
-            }
-        }
-        return joukko;
-    }
-
-    public IntJoukko leikkaus(IntJoukko b) {
-        IntJoukko tama = new IntJoukko();
-        tama.setLukujono(lukujono);
-        tama.setAlkioidenLkm(alkioidenLkm);
-        return leikkaa(tama,b);
-    }
-    
-    private IntJoukko leikkaa(IntJoukko joukko, IntJoukko toinenJoukko){
-        for (int i = 0; i < lukujono.length; i++) {
-            if(!toinenJoukko.isInArray(lukujono[i])){
-                joukko.poista(lukujono[i]);
-            }
-        }
-        return joukko;
-    }
-    
-    public IntJoukko erotus (IntJoukko b) {
-        IntJoukko tama = new IntJoukko();
-        tama.setLukujono(lukujono);
-        tama.setAlkioidenLkm(alkioidenLkm);
-        return erota(tama,b.getLukujono());
-    }
-    
-    private IntJoukko erota(IntJoukko joukko, int [] toinenLukujono){
-       for (int i = 0; i < toinenLukujono.length; i++) {
-            if(isInArray(toinenLukujono[i])){
-                joukko.poista(toinenLukujono[i]);
-            }
-        }
-        return joukko;
     }
         
 }
