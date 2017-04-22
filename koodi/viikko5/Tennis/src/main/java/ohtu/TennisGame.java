1803b0f263de3128279b06f8997bd1529a964bc7
villepaa <ville.paavola@gmail.com>
2017-04-22 21:49:39 +0300

    viikko5 valmis

--- koodi/viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ koodi/viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -6,6 +6,7 @@ public class TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private String [] points = {"Love","Fifteen","Thirty","Forty"};
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -21,64 +22,91 @@ public class TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2){
-            score = selectStandingsAtEvenCase();
+        score = setScore(isEven(),onePlayerHasFortyOrMore(),score);
+        return score;
+    }
+    
+    private String setScore(boolean even, boolean fortyOrMore, String score){
+        if (even){
+            score = getScoreAtEvenCase(m_score1,score);
+        }else if (fortyOrMore){
+            score = getScoreAtDecivePhase(score);
+        }else{
+            score = setScoreAtNormalCase(m_score1,m_score2,score); 
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return score;
+    } 
+    
+    private String getScoreAtDecivePhase(String score){
+        if (advantage1() || advantage2()){
+            score = setAdvantage(score);
+        }else {
+            score = setWinner(score);
         }
         return score;
     }
     
-    private String selectStandingsAtEvenCase(){
-        String score = "";
-        switch (m_score1){
-            
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-            }
+    private String setAdvantage(String score){
+        if (advantage1()){
+            score ="Advantage player1";
+        }else if (advantage2()){
+            score ="Advantage player2";
+        }
         return score;
+    }
+    
+    private String setWinner(String score){
+        if (scoreDifference()>=2){
+            score = "Win for player1";
+        }else {
+            score ="Win for player2";
+        }
+        return score;
+    }
+    
+    private String setScoreAtNormalCase(int score1,int score2,String score){
+        score = setScore1(score1,score)+ "-";
+        score = setScore2(score2,score);
+        return score;                
+    }
+    
+    private String setScore1(int score1, String score){
+        score += points[score1];
+        return score;
+    }
+    
+    private String setScore2(int score1, String score){
+        score += points[score1];
+        return score;
+    }
+    
+    private String getScoreAtEvenCase(int score1, String score){
+        if(onePlayerHasFortyOrMore()){
+            score = "Deuce";
+        }else{
+            score += points[score1] + "-All";
+        }        
+        return score;
+    }
+    
+    
+    private int scoreDifference(){
+        return m_score1-m_score2;   
+    }
+    
+    private boolean advantage1(){
+        return scoreDifference() == 1;
+    }
+    
+    private boolean advantage2(){
+        return scoreDifference() == -1;
+    }
+    
+    private boolean onePlayerHasFortyOrMore(){
+        return m_score1>=points.length || m_score2>=points.length;
+    }
+    
+    private boolean isEven(){
+        return m_score1==m_score2;
     }
 }
