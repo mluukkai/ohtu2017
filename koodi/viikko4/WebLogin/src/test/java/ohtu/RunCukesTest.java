a4905d61a80d1baaff24a999cadb6046d6f4ec90
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-26 18:02:30 +0300

    Merged with kurssirepo

---
+++ koodi/viikko4/WebLogin/src/test/java/ohtu/RunCukesTest.java
@@ -0,0 +1,13 @@
+package ohtu;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCukesTest {
+    @ClassRule
+    public static ServerRule server = new ServerRule(4567);
+}
---
+++ koodi/viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,18 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
+          
+        Matcher m = new And( new HasAtLeast(10, "goals"),
+                             new HasAtLeast(10, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println( player );
+        }
+    }
+}
