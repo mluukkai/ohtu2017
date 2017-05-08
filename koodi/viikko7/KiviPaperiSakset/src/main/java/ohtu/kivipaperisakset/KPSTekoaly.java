f06af83c9f717250a05d482a67c841e6b6e90461
Leevi Heino <leeviheino@live.fi>
2017-05-08 19:03:45 +0300

    asd

---
+++ koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
@@ -0,0 +1,43 @@
+package ohtu.kivipaperisakset;
+
+import java.util.Scanner;
+
+public class KPSTekoaly {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public void pelaa() {
+        Tuomari tuomari = new Tuomari();
+        Tekoaly tekoaly = new Tekoaly();
+
+        System.out.print("Ensimmäisen pelaajan siirto: ");
+        String ekanSiirto = scanner.nextLine();
+        String tokanSiirto;
+
+        tokanSiirto = tekoaly.annaSiirto();
+        System.out.println("Tietokone valitsi: " + tokanSiirto);
+
+
+        while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
+            tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
+            System.out.println(tuomari);
+            System.out.println();
+
+            System.out.print("Ensimmäisen pelaajan siirto: ");
+            ekanSiirto = scanner.nextLine();
+
+            tokanSiirto = tekoaly.annaSiirto();
+            System.out.println("Tietokone valitsi: " + tokanSiirto);
+            tekoaly.asetaSiirto(ekanSiirto);
+
+        }
+
+        System.out.println();
+        System.out.println("Kiitos!");
+        System.out.println(tuomari);
+    }
+
+    private static boolean onkoOkSiirto(String siirto) {
+        return "k".equals(siirto) || "p".equals(siirto) || "s".equals(siirto);
+    }
+}
